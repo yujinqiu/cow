07b4111a3825cc8a6951478ca85fda22cab9b086
Jinqiu Yu <yujinqiuustb@gmail.com>
2017-04-01 10:04:04 +0800

    fix leading / path missing error

--- proxy.go
+++ proxy.go
@@ -363,7 +363,7 @@ func (c *clientConn) serveSelfURL(r *Request) (err error) {
 	if r.Method != "GET" {
 		goto end
 	}
-	if r.URL.Path == "/pac" || r.URL.Path == "proxy.pac" || r.URL.Path == "wpad.dat" || strings.HasPrefix(r.URL.Path, "/pac?") {
+	if r.URL.Path == "/pac" || r.URL.Path == "/proxy.pac" || r.URL.Path == "/wpad.dat" || strings.HasPrefix(r.URL.Path, "/pac?") {
 		sendPAC(c)
 		// PAC header contains connection close, send non nil error to close
 		// client connection.
